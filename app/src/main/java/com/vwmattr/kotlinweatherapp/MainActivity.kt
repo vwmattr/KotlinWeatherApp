50fb1062c84dbe3ceb1107b751379851361a25b9
Matt R <vwmattr@gmail.com>
2016-08-02 23:02:19 -0400

    Added a toast message to MainActivity.

--- app/src/main/java/com/vwmattr/kotlinweatherapp/MainActivity.kt
+++ app/src/main/java/com/vwmattr/kotlinweatherapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.vwmattr.kotlinweatherapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         message.text = "Hello Kotlin!"
+
+        niceToast(message = "Chapter 4 rocks")
+    }
+
+    fun niceToast(message: String,
+                  tag: String = MainActivity::class.java.simpleName,
+                  length: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(this, "[$tag] $message", length).show()
     }
 }
