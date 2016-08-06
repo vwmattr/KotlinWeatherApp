276ad1630dd819b348e71e4a8eb68f67f8750528
Matt R <vwmattr@gmail.com>
2016-08-06 19:58:59 -0400

    Override some operators in ForecastList and update ForecastListAdapter to use the new operator overrides.

--- app/src/main/java/com/vwmattr/kotlinweatherapp/domain/model/DomainClasses.kt
+++ app/src/main/java/com/vwmattr/kotlinweatherapp/domain/model/DomainClasses.kt
@@ -5,7 +5,10 @@ package com.vwmattr.kotlinweatherapp.domain.model
  */
 data class ForecastList(val city: String,
                         val country: String,
-                        val dailyForecast:List<Forecast>)
+                        val dailyForecast:List<Forecast>) {
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+    fun size(): Int = dailyForecast.size
+}
 
 data class Forecast(val date: String,
                     val description: String,
